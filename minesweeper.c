1061e241aa10cf576a054dacffbf62588366bfd3
mehrandvm <mehrandvmdev@gmail.com>
2018-12-13 16:21:37 +0330

    First Launch Ubuntu

--- minesweeper.c
+++ minesweeper.c
@@ -24,57 +24,73 @@ int CheckY(int yUser);
 int main()
 {
     createMap();
-    getRandX();
-    getRandY();
     int i;
-
-    for(i=0;i<10;i++) printf("%d  ",arrX[i]);
-    printf("\n");
-    for(i=0;i<10;i++) printf("%d  ",arrY[i]);
-    printf("\n");
-    
 
     char oneInput[3];
     int firstInput;
     int xUser, yUser;
+    int arrX, arrY;
 
     while (1)
-    {   
+    {
         printMap();
-        fgets(oneInput,4,stdin);
+
+        for (i = 0; i < MineNum; i++)
+        {
+            n = rand() % 30 + 1;
+            printf("X: %d  ", n);
+            arrX[i] = n;
+        }
+
+        printf("\n");
+
+        int arrY[MineNum];
+        for (i = 0; i < MineNum; i++)
+        {
+            n = rand() % 14 + 1;
+            printf("Y: %d  ", n);
+            arrY[i] = n;
+        }
+        printf("\n");
+
+        //fgets(oneInput,4,stdin);
         printf("string is: %s\n", oneInput);
         scanf("%d", &firstInput);
         scanf("%d", &xUser);
         scanf("%d", &yUser);
-        
+
         if (firstInput == 9090)
         {
             clearscreen();
             printf("Check");
-            if (CheckX(xUser) == 1 && CheckY(yUser) == 1) printf("BOOM\n");
+            if (CheckX(xUser) == 1 && CheckY(yUser) == 1)
+                printf("BOOM\n");
         }
         else if (oneInput == "cho")
         {
             clearscreen();
-            if(Minefield[yUser-1][xUser-1]=='P') printf("Already Flagged!\n");
-            Minefield[yUser-1][xUser-1]='P';
+            if (Minefield[yUser - 1][xUser - 1] == 'P')
+                printf("Already Flagged!\n");
+            Minefield[yUser - 1][xUser - 1] = 'P';
         }
         else if (firstInput == 9292)
         {
             clearscreen();
-            if(Minefield[yUser-1][xUser-1]=='P') printf("Its not Flagged!\n");
-            Minefield[yUser-1][xUser-1]='#';
+            if (Minefield[yUser - 1][xUser - 1] == 'P')
+                printf("Its not Flagged!\n");
+            Minefield[yUser - 1][xUser - 1] = '#';
         }
-        
     }
 }
 
-void clearscreen(){
+void clearscreen()
+{
     system("cls");
 }
 
-void createMap(){
-    int i,j;
+void createMap()
+{
+    int i, j;
     for (i = 0; i < Y; i++)
     {
         for (j = 0; j < X; j++)
@@ -88,7 +104,6 @@ void printMap()
 {
 
     int i, j;
-
 
     for (i = 0; i <= X; i++)
     {
@@ -114,40 +129,13 @@ void printMap()
     }
 }
 
-int getRandX()
-{
-    int i, n;
-    int arrX[MineNum];
-    for (i = 0; i < MineNum; i++)
-    {
-        n = rand() % 30 + 1;
-        printf("X: %d  ", n);
-        arrX[i]=n;
-    }
-    printf("\n");
-    return arrX;
-}
-
-int getRandY()
-{
-    int i, n;
-    int arrY[MineNum];
-    for (i = 0; i < MineNum; i++)
-    {
-        n = rand() % 14 + 1;
-        printf("Y: %d  ", n);
-        //arrY[i]=n;
-    }
-    printf("\n");
-    //return arrY[MineNum];
-}
-
 int CheckX(int xUser)
 {
     int i;
     for (i = 0; i < 10; i++)
     {
-        if(xUser == arrX[i]){
+        if (xUser == arrX[i])
+        {
             printf("X yes\n");
             return 1;
         }
@@ -160,7 +148,8 @@ int CheckY(int yUser)
     int i;
     for (i = 0; i < 10; i++)
     {
-        if(yUser == arrY[i]){
+        if (yUser == arrY[i])
+        {
             printf("Y yes\n");
             return 1;
         }
